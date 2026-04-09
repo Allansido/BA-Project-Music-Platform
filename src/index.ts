62970ad88e8a48840a712e654d14550a1f258940
sido <allansido69@gmail.com>
2026-04-09 12:47:26 +0200

    The dataset loader is working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { loadLastFMDataset } from "./data/lastfmLoader";
 import { saveInteractionsToJSON } from "./data/dataPreprocessor";
 
-const filePath =
-    "dataset/raw/userid-timestamp-artid-artname-traid-traname.tsv";
+const filePath = "dataset/raw/userid-timestamp-artid-artname-traid-traname.tsv";
 
 async function run() {
     const interactions = await loadLastFMDataset(filePath, 100);
+
+
+    const uniqueUsers = new Set(interactions.map(i => i.userId));
+    console.log("Unique users:", uniqueUsers.size);
+
 
     saveInteractionsToJSON(interactions, "dataset/processed/interactions.json");
 }
