af3176737cc9d3ebb2fceb41246ca1d04205353a
sido <allansido69@gmail.com>
2026-05-19 08:32:36 +0200

    Remove generated and dependency files from tracking

--- dist/presentation/routes/recommendationRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const recommendationController_1 = require("../controllers/recommendationController");
-const router = (0, express_1.Router)();
-router.get("/", recommendationController_1.getRecommendations);
-exports.default = router;
